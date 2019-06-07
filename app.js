23d1ee91575ecf87759ad5dbdc4abf69c66dbdbd
Som Ramnani <som.ramnani@gmail.com>
2019-06-07 14:15:27 -0500

    Added passport user authentication

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require ("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require('body-parser');
+var exphbs = require("express-handlebars");
+var expressValidator  = require("express-validator");
+var flash = require("connect-flash");
+var sessions = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+mongoose = connect("mongodb://localhost/loginapp")
+
+var db = mongoose.connection;
+  
