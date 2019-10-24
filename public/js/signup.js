da54480d99a050ef5e216bacd5fea743d364b1d3
Som Ramnani <som.ramnani@gmail.com>
2019-10-24 16:24:11 -0500

    edited comments and added gitignore

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // When the signup button is clicked, validates the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
@@ -15,22 +15,24 @@ $(document).ready(function() {
     if (!userData.email || !userData.password) {
       return;
     }
-    // If we have an email and password, run the signUpUser function
+
     signUpUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
 
-  // Does a post to the signup route. If succesful, we are redirected to the members page
-  // Otherwise we log any errors
+  // Does a post to the signup route. If succesful, redirect to the members page
+  // Otherwise log any errors
   function signUpUser(email, password) {
     $.post("/api/signup", {
       email: email,
       password: password
-    }).then(function(data) {
-      window.location.replace(data);
-      // If there's an error, handle it by throwing up a boostrap alert
-    }).catch(handleLoginErr);
+    })
+      .then(function(data) {
+        window.location.replace(data);
+        // If there's an error, handle it by throwing up a boostrap alert
+      })
+      .catch(handleLoginErr);
   }
 
   function handleLoginErr(err) {
